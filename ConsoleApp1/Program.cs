e057d6065790728f7b9b9e86e052b3fbea83e68d
dev-abdo-salem <modysalm1973s@gmail.com>
2023-12-06 19:46:18 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inits;
+            inits = Initials("Michael", "Mason", "McMillan");
+            Console.WriteLine(inits);
+
+        }
+        public static string Initials(string fname, string mname, string lname)
+        {
+            return (String.Format("{0}{1}{2}", fname.Substring(0, 1),
+            mname.Substring(0, 1), lname.Substring(0, 1)));
+        }
+    }
+}
